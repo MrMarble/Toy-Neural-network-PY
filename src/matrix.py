5dcd930d27fb1becb283efa25bf61965607c20a1
MrMarble <4268580+MrMarble@users.noreply.github.com>
2018-02-13 17:07:27 +0100

    Added methods randomize, print and static_map, renamed multiply to static_multiply and added instance multiply

--- src/matrix.py
+++ src/matrix.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random as rnd
 
 
 class Matrix():
@@ -10,25 +11,29 @@ class Matrix():
 
     @staticmethod
     def from_list(arr: list):
-        return Matrix(len(arr), 1).map(lambda _, i, j: arr[i])
+        return Matrix(len(arr), 1).map(lambda e, i, j: arr[i])
 
     @staticmethod
     def subtract(matrix_a, matrix_b):
         if matrix_a.rows is not matrix_b.rows or matrix_a.cols is not matrix_b.cols:
             print('Columns and Rows of A must match Columns and Rows of B.')
             return
-        return Matrix(matrix_a.rows, matrix_a.cols).map(lambda _, i, j: matrix_a.data[i][j] - matrix_b.data[i][j])
+        return Matrix(matrix_a.rows, matrix_a.cols).map(lambda e, i, j: matrix_a.data[i][j] - matrix_b.data[i][j])
 
     @staticmethod
-    def multiply(matrix_a, matrix_b):
+    def static_multiply(matrix_a, matrix_b):
         if matrix_a.cols is not matrix_b.rows:
             print('Columns of A must Math rows of B.')
             return
-        return Matrix(matrix_a.rows, matrix_b.cols).map(lambda _, i, j: sum([matrix_a.data[i][k] * matrix_b.data[k][j] for k in range(matrix_a.cols)]))
+        return Matrix(matrix_a.rows, matrix_b.cols).map(lambda e, i, j: sum([matrix_a.data[i][k] * matrix_b.data[k][j] for k in range(matrix_a.cols)]))
+
+    @staticmethod
+    def static_map(matrix, func):
+        return Matrix(matrix.rows, matrix.cols).map(lambda e, i, j: func(matrix.data[i][j], i, j))
 
     @staticmethod
     def transpose(matrix):
-        return Matrix(matrix.cols, matrix.rows).map(lambda _, i, j: matrix.data[j][i])
+        return Matrix(matrix.cols, matrix.rows).map(lambda e, i, j: matrix.data[j][i])
 
     def add(self, n: int):
         if type(n) is Matrix:
@@ -39,6 +44,15 @@ class Matrix():
         else:
             return self.map(lambda e, i, j: e + n)
 
+    def multiply(self, matrix_a):
+        if type(matrix_a) is Matrix:
+            if self.rows is not matrix_a.rows or self.cols is not matrix_a.cols:
+                print('Columns and Rows of A must match Columns and Rows of B.')
+                return
+            return self.map(lambda e, i, j: e * matrix_a.data[i][j])
+        else:
+            return self.map(lambda e, i, j: e * matrix_a)
+
     def map(self, func):
         for row in range(self.rows):
             for col in range(self.cols):
@@ -46,5 +60,12 @@ class Matrix():
                 self.data[row][col] = func(tmp, row, col)
         return self
 
+    def randomize(self):
+        return self.map(lambda e, i, j: rnd.random() * 2 - 1)
+
     def to_list(self):
         return np.array(self.data).flatten()
+
+    def print(self):
+        print(self.data)
+        return self
