b5921658f4a1a96b31a9f99f166d71fde6595b03
MrMarble <4268580+MrMarble@users.noreply.github.com>
2018-02-13 15:50:34 +0100

    Removed more redundant code

--- src/test_matrix.py
+++ src/test_matrix.py
@@ -1,5 +1,6 @@
 import pytest
 from matrix import Matrix
+import json
 
 
 @pytest.fixture()
@@ -10,6 +11,7 @@ def init_matrix():
     m.data[2] = [7, 8, 9]
     return m
 
+
 @pytest.fixture()
 def init_matrix_10():
     m = Matrix(3, 3)
@@ -17,6 +19,13 @@ def init_matrix_10():
     m.data[1] = [13, 14, 15]
     m.data[2] = [16, 17, 18]
     return m
+
+
+def compare(matrix_a, matrix_b):
+    assert matrix_a.rows == matrix_b.rows
+    assert matrix_a.cols == matrix_b.cols
+    assert (matrix_a.data == matrix_b.data).all()
+
 
 def test_add_scalar_to_matrix(init_matrix):
     init_matrix.add(1)
@@ -26,9 +35,7 @@ def test_add_scalar_to_matrix(init_matrix):
     expected.data[1] = [5, 6, 7]
     expected.data[2] = [8, 9, 10]
 
-    assert init_matrix.rows == expected.rows
-    assert init_matrix.cols == expected.cols
-    assert (init_matrix.data == expected.data).all()
+    compare(init_matrix, expected)
 
 
 def test_add_matrix_to_matrix(init_matrix, init_matrix_10):
@@ -39,9 +46,7 @@ def test_add_matrix_to_matrix(init_matrix, init_matrix_10):
     expected.data[1] = [17, 19, 21]
     expected.data[2] = [23, 25, 27]
 
-    assert init_matrix.rows == expected.rows
-    assert init_matrix.cols == expected.cols
-    assert (init_matrix.data == expected.data).all()
+    compare(init_matrix, expected)
 
 
 def test_subtract_matrix_from_matrix(init_matrix, init_matrix_10):
@@ -52,6 +57,23 @@ def test_subtract_matrix_from_matrix(init_matrix, init_matrix_10):
     expected.data[1] = [9, 9, 9]
     expected.data[2] = [9, 9, 9]
 
-    assert result.rows == expected.rows
-    assert result.cols == expected.cols
-    assert (result.data == expected.data).all()
+    compare(result, expected)
+
+
+def test_from_list():
+    result = Matrix.from_list([0, 0, 0])
+    expected = Matrix(3, 1)
+
+    compare(result, expected)
+
+
+def test_transpose():
+    m = Matrix(2, 2)
+    m.data[0] = [1, 2]
+    m.data[1] = [3, 4]
+    m = Matrix.transpose(m)
+
+    expected = Matrix(2, 2)
+    expected.data[0] = [1, 3]
+    expected.data[1] = [2, 4]
+    compare(m, expected)
