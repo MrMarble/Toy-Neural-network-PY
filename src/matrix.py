bffd0eba6692379b4b85a1cfe696c5b003848232
Alvaro <alvarotinocomarmol@gmail.com>
2018-02-13 01:07:46 +0100

    Added add, subtract and map methods to the Matrix class and its tests

---
+++ src/matrix.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+class Matrix():
+    
+    def __init__(self, rows: int, cols: int):
+        self.rows = rows
+        self.cols = cols
+        self.data = np.zeros((rows, cols))
+
+    def add(self, n: int):
+        if type(n) is Matrix:
+            if self.rows is not n.rows or self.cols is not n.cols:
+                print('Columns and Rows of A must match Columns and Rows of B.')
+                return
+            return self.map(lambda e,i,j: e+n.data[i][j])
+        else:
+            return self.map(lambda e,i,j: e + n)
+    
+    def substract(a, b):
+        if a.rows is not b.rows or a.cols is not b.cols:
+            print('Columns and Rows of A must match Columns and Rows of B.')
+            return
+        return Matrix(a.rows, a.cols).map(lambda _, i, j: a.data[i][j] - b.data[i][j])
+
+    def map(self, func):
+        for row in range(self.rows):
+            for col in range(self.cols):
+                tmp = self.data[row][col]
+                self.data[row][col] = func(tmp, row, col)
+        return self
---
+++ src/test_matrix.py
@@ -0,0 +1,53 @@
+import pytest
+from matrix import Matrix
+
+
+def test_add_scalar_to_matrix():
+    m = Matrix(3, 3)
+    m.data[0] = [1, 2, 3]
+    m.data[1] = [4, 5, 6]
+    m.data[2] = [7, 8, 9]
+    m.add(1)
+
+    expected = Matrix(3, 3)
+    expected.data[0] = [2, 3, 4]
+    expected.data[1] = [5, 6, 7]
+    expected.data[2] = [8, 9, 10]
+
+    assert m.rows == expected.rows
+    assert m.cols == expected.cols
+    assert (m.data == expected.data).all()
+
+def test_add_matrix_to_matrix():
+    m = Matrix(2, 2)
+    m.data[0] = [1,2]
+    m.data[1] = [3,4]
+    n = Matrix(2, 2)
+    n.data[0] = [10, 11]
+    n.data[1] = [12, 13]
+    m.add(n)
+
+    expected = Matrix(2, 2)
+    expected.data[0] = [11, 13]
+    expected.data[1] = [15, 17]
+
+    assert m.rows == expected.rows
+    assert m.cols == expected.cols
+    assert (m.data == expected.data).all()
+
+def test_subtract_matrix_from_matrix():
+    m = Matrix(2, 2)
+    m.data[0] = [10, 11]
+    m.data[1] = [12, 13]
+    n = Matrix(2, 2)
+    n.data[0] = [1,2]
+    n.data[1] = [3,4]
+    m_minus_n = Matrix.substract(m, n)
+
+    expected = Matrix(2, 2)
+    expected.data[0] = [9, 9]
+    expected.data[1] = [9, 9]
+
+    assert m_minus_n.rows == expected.rows
+    assert m_minus_n.cols == expected.cols
+    assert (m_minus_n.data == expected.data).all()
